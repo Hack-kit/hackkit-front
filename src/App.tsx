c49001e0845cf9a5662cdf2b2adeb44f21a08762
AiRini <tjdwp1999@gmail.com>
2021-09-24 23:40:16 +0900

    feat: 라우팅 및 기본 세팅 완료
    라우팅, 테마, css normalize추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  ThemeProvider,
+  StyledEngineProvider,
+  createTheme,
+  CssBaseline,
+} from '@mui/material';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={createTheme()}>
+      <CssBaseline />
+      <StyledEngineProvider injectFirst>
+        <BrowserRouter>
+          <Route path="/" exact>
+            <div>main</div>
+          </Route>
+        </BrowserRouter>
+      </StyledEngineProvider>
+    </ThemeProvider>
   );
 }
 
