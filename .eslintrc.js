63e8dfc300f5222af2c0cf369be88a6e81d064ed
AiRini <tjdwp1999@gmail.com>
2021-09-24 23:39:23 +0900

    build: 스타일 및 코드 퀄리티 패키지 추가
    prettier, eslint, airbnb, mui 추가
    react-router, lodash, moment추가

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', 'react', '@typescript-eslint'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'no-console': 'off',
+  },
+  ignorePatterns: ['reportWebVitals.ts', '*.js', '*.jsx'],
+};
---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const emotionPresetOptions = {};
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const emotionBabelPreset = require('@emotion/babel-preset-css-prop').default(
+  undefined,
+  emotionPresetOptions,
+);
+
+module.exports = {
+  babel: {
+    plugins: [...emotionBabelPreset.plugins],
+  },
+};
