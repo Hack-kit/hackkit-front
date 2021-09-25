784d7c2c769809a278af3c1cfc714e22bcc70be2
soojin <sobeast742@gmail.com>
2021-09-25 11:47:38 +0900

    Button

---
+++ src/component/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Button } from "@material-ui/core";
+
+
+export default ({children}) => (
+    <Button variant="contained" color="primary">{children}</Button>
+  );
